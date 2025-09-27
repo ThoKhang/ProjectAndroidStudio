333768a6a7d4fc878135d56979c905a611ccbf7e
ThoKhang <khangheheqt@gmail.com>
2025-09-27 21:01:29 +0700

    fix loi vat

--- app/src/main/java/com/example/musicapp/GiaoDienDangNhap.java
+++ app/src/main/java/com/example/musicapp/GiaoDienDangNhap.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class GiaoDienDangNhap extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_man_hinh_vao);
+        setContentView(R.layout.activity_giao_dien_dang_nhap);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
